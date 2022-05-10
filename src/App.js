5f7b1932f5ffb3eb6a5d70a18c93f0b522ad2d10
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-10 13:00:50 +0430

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const Home = () => <h1>Home</h1>;
+const Blog = () => <h1>Blog</h1>;
 
 function App() {
   return (
     <Router>
-      <Route path="/">
-        <Home />
-      </Route>
+      <Route exact path="/" component={Home} />
     </Router>
   );
 }
